892b20c10590760892a511adb82b153e2743a6e8
kddemir04 <240541007@firat.edu.tr>
2025-10-20 12:43:18 +0300

    Update MilKilometreTablosu.java

---
+++ Task-5-TabloDesenleri/MilKilometreTablosu.java
@@ -0,0 +1,14 @@
+public class MilKilometreTablosu {
+
+    public static void main(String[] args) {
+
+        //Donusum Tablosu
+        System.out.println("mil        km");
+        System.out.println("------------");
+        System.out.println("1 mil    1.609km");
+        System.out.println("5 mil " + "   " + (1.609 * 5) + " km ");
+        System.out.println("10 mil " + "  " +  (1.609 * 10) + " km ");
+        System.out.println("20 mil " + "  " + (1.609 * 20) + " km ");
+        System.out.println("50 mil " + "  " + (1.609 * 50) + " km ");
+    }
+}
