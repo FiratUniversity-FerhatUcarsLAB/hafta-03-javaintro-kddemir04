53dc4bd256a7e2e49e4c043eed227aebdfa1cf39
kddemir04 <240541007@firat.edu.tr>
2025-10-20 12:46:24 +0300

    Update AsciiArtIsim.java

---
+++ TASK-BONUS/AsciiArtIsim.java
@@ -0,0 +1,15 @@
+public class AsciiArtIsim {
+
+    public static void main(String[] args) {
+
+
+        //ASCII Sanat BONUS4
+
+        System.out.println("#   #   ##    ####    . .   ####");
+        System.out.println("#  #   #  #   #   #   # #   #   #");
+        System.out.println("# #   #    #  #    #  # #   #   #");
+        System.out.println("##    ######  #    #  # #   ####");
+        System.out.println("# #   #    #  #   #   # #   #  #");
+        System.out.println("#  #  #    #  ####    # #   #   #");
+    }
+}
