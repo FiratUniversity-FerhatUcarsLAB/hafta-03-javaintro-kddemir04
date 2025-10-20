03f899d69538064450de1036834497aabf4e9022
kddemir04 <240541007@firat.edu.tr>
2025-10-20 12:33:11 +0300

    Update DikdortgenHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DikdortgenHesap.java
@@ -0,0 +1,6 @@
+
+        //Dikdortgen icin
+        double en = 4.5;
+        double boy = 7.9;
+        System.out.println("\nDikdörtgenin alanı: " + (en*boy));
+        System.out.println("Dikdörtgenin çevresi: " + (2*(en+boy)));
