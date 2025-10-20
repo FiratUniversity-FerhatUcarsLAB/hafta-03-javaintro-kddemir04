e960b627d150d7554dc9be730e1a962d0cebd008
kddemir04 <240541007@firat.edu.tr>
2025-10-20 12:39:48 +0300

    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,18 @@
+public class IsimKarti {
+
+    public static void main(String[] args) {
+
+        //Isim karti
+        String ad = "Abdulkadir";
+        String soyad = "Demir";
+        String ogrenciNo = "240541007";
+        String bolum = "Yazılım mühendisliği";
+        System.out.println("====================");
+        System.out.println("ÖĞRENCİ BİLGİLERİ");
+        System.out.println("Ad: " + ad);
+        System.out.println("Soyad: " + soyad);
+        System.out.println("No: " + ogrenciNo);
+        System.out.println("Bölüm: " + bolum);
+        System.out.println("====================");
+    }
+}
