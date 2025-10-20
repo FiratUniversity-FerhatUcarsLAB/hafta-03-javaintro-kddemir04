76c714aadda09f06841817ed8a4b37d9156aaa94
kddemir04 <240541007@firat.edu.tr>
2025-10-20 12:32:31 +0300

    Update DaireHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DaireHesap.java
@@ -0,0 +1,12 @@
+public class DaireHesap {
+
+    public static void main(String[] args) {
+
+        //Daire icin
+        double r = 5.5;
+        double alan = Math.PI * r * r;
+        double cevre = 2 * Math.PI * r;
+        System.out.println("Dairenin alanı: " + alan);
+        System.out.println("Dairenin çevresi: " + cevre);
+    }
+}
