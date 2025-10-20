6e423d8a2d90d660f886e976df77890abac12068
kddemir04 <240541007@firat.edu.tr>
2025-10-20 12:38:59 +0300

    Update HesapOzeti.java

---
+++ Task-4-StringDesenler/HesapOzeti.java
@@ -0,0 +1,19 @@
+public class HesapOzeti {
+
+    public static void main(String[] args) {
+
+        //Hesap Ozeti
+        // Başlıklar
+        System.out.println("Ürün\tMiktar\tFiyat");
+        System.out.println("------------------------");
+
+        // Ürünler
+
+        System.out.println("Elma     \t2\t10");
+        System.out.println("Ekmek     \t1\t5");
+        System.out.println("Süt       \t3\t7");
+        System.out.println("Çikolata\t5\t4");
+        System.out.println("\nHesap: " + (2*10 + 1*5 + 3*7 + 5*4) + " TL ");
+
+    }
+}
