2b0e24327c619dc67d95c9752e2c8b97d6aaa7b3
kddemir04 <240541007@firat.edu.tr>
2025-10-20 12:25:59 +0300

    Update ToplamaSeri.java

---
+++ Task-2-Matematikselislemler/ToplamaSeri.java
@@ -0,0 +1,13 @@
+public class ToplamaSeri {
+
+    public static void main(String[] args) {
+
+        //Toplama serisi
+        int sonuc = 1+2+3+4+5+6+7+8+9;
+        System.out.println("1+2+3+4+5+6+7+8+9 = " + sonuc);
+
+
+
+
+    }
+}
