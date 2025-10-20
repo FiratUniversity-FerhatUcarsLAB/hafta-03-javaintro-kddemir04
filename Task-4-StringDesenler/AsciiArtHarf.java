3a0c9924b05cc2a65445590e258e7da387a0ba6e
kddemir04 <240541007@firat.edu.tr>
2025-10-20 12:38:03 +0300

    Update AsciiArtHarf.java

---
+++ Task-4-StringDesenler/AsciiArtHarf.java
@@ -0,0 +1,17 @@
+public class AsciiArtHarf {
+
+    public static void main(String[] args) {
+
+
+        //ASCII Art
+        System.out.println("K    K");
+        System.out.println("K   K");
+        System.out.println("K  K");
+        System.out.println("K K");
+        System.out.println("KK");
+        System.out.println("K K");
+        System.out.println("K  K");
+        System.out.println("K   K");
+        System.out.println("K    K");
+    }
+}
