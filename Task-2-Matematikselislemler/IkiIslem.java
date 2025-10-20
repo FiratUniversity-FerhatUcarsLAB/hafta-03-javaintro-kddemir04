db795a9aadb59cef0f4a2b24be002f498553c333
kddemir04 <240541007@firat.edu.tr>
2025-10-20 12:25:12 +0300

    Update IkiIslem.java

---
+++ Task-2-Matematikselislemler/IkiIslem.java
@@ -0,0 +1,13 @@
+public class ikiIslem {
+
+    public static void main(String[] args) {
+
+        //Iki islem sonucu
+        double pay = 9.5 * 4.5 - 2.5 * 3;
+        double payda = 45.5 - 3.5;
+        double bolum = pay / payda;
+        System.out.println("Bölümün sonucu: " + bolum);
+        System.out.printf("%.4f", bolum);
+
+    }
+}
